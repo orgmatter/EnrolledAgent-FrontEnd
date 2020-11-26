ca459d6f5616757a00f66f124b4d3fb247a9071a
Yinkash100 <yinkash100@gmail.com>
2020-11-26 09:53:49 +0100

    about to start responsiveness

--- public/js/signup-form-validator.js
+++ public/js/signup-form-validator.js
@@ -1,60 +1,62 @@
-// console.log('about to check the shii')
-// name, email, phoneNumber, message
-
-console.log('is she a lawyer?')
-
 function validateNonEmpty(inputId) {
-  console.log('He don happen pen pen ren pen')
-  const fieldContent = document.getElementById(inputId).value.trim;
-  if (fieldContent === '') {
-    document.getElementById(`${inputId}-validator`).innerHTML = 'Name is required';
+  const fieldContent = document.getElementById(inputId).value.trim();
+  if (fieldContent === "") {
+    document.getElementById(
+      `${inputId}-validator`
+    ).innerHTML = `${inputId} is required`;
     return false;
   } else {
-    document.getElementById(`${inputId}-validator`).innerHTML = '';
+    document.getElementById(`${inputId}-validator`).innerHTML = "";
     return true;
   }
 }
 
-function validateName() {
-  var nameFieldContent = document.getElementById('name').value;
-  if (nameFieldContent === '') {
-    document.getElementById('nameValidator').innerHTML = 'Name is required';
+function validateEmail(emailInputId) {
+  const emailFieldContent = document.getElementById(emailInputId).value;
+  if (validateNonEmpty(emailInputId)) {
+    if (!validator.isEmail(emailFieldContent)) {
+      document.getElementById(`${emailInputId}-validator`).innerHTML =
+        "Please enter a valid email";
+      return false;
+    } else {
+      document.getElementById(`${emailInputId}-validator`).innerHTML = "";
+      return true;
+    }
+  }
+}
+
+function validateConfirmPassword(inputId) {
+  const fieldContent = document.getElementById(inputId).value.trim();
+  const passwordFieldContent = document.getElementById("password").value.trim();
+  if (fieldContent !== passwordFieldContent || passwordFieldContent === '') {
+    document.getElementById(`${inputId}-validator`).innerHTML =
+      "Passwords must match";
     return false;
   } else {
-    document.getElementById('nameValidator').innerHTML = '';
+    document.getElementById(`${inputId}-validator`).innerHTML = "";
     return true;
   }
 }
 
-function validateEmail() {
-  var emailFieldContent = document.getElementById('email').value;
-  if (emailFieldContent === '') {
-    document.getElementById('emailValidator').innerHTML = 'Email is required';
-    return false;
-  } else if (!validator.isEmail(emailFieldContent)) {
-    document.getElementById('emailValidator').innerHTML =
-      'Please enter a valid email';
-    return false;
-  } else {
-    document.getElementById('emailValidator').innerHTML = '';
-    return true;
+function validateSignupForm() {
+  if (
+    !(
+      !validateNonEmpty("firstname") &&
+      !validateNonEmpty("lastname") &&
+      !validateEmail("email") &&
+      !validateNonEmpty("password") &&
+      !validateConfirmPassword("confirmPassword")
+    )
+  ) {
+    document.getElementById("signup-submit-btn").click();
   }
 }
 
-function validateMessage() {
-  var messageFieldContent = document.getElementById('message').value;
-  if (messageFieldContent === '') {
-    document.getElementById('messageValidator').innerHTML =
-      'Message is required';
-    return false;
-  } else {
-    document.getElementById('messageValidator').innerHTML = '';
-    return true;
-  }
-}
-
-function validateAll() {
-  if (validateName() && validateEmail() && validateMessage()) {
-    document.getElementById('submit-button').click();
+function validateLoginForm(){
+  if(!(
+    !validateEmail("email") &&
+    !validateNonEmpty('password')
+  )){
+    document.getElementById("login-submit-btn").click();
   }
 }
