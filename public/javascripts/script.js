40bc2a165443c2ecf15f67ea38d1c7e47542a435
joshmoy <obodaij@gmail.com>
2021-01-30 00:34:12 +0100

    minified scripts

--- public/javascripts/script.js
+++
@@ -1,8 +0,0 @@
-// alert("here");
-// const btn = document.querySelector(".btn");
-// const txt = document.querySelector(".login");
-
-// btn.addEventListener("click", () => {
-//   txt.textContent = "Helo!";
-//   txt.style.fontSize = "50px";
-// });
