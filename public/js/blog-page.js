1cbcd6260ef08793346c10a6ffed60ab915ce744
joshmoy <obodaij@gmail.com>
2021-02-03 15:10:27 +0100

    blog fix

--- public/js/blog-page.js
+++ public/js/blog-page.js
@@ -1,1 +1,21 @@
-const blogArticleBody=document.getElementById("blog-article-body"),articleRule=document.getElementById("article-rule"),blogDiv=document.querySelector("#blog-main"),bgImg=blogDiv.getAttribute("bg-img");blogDiv.style.backgroundImage=bgImg;blogDiv&&(blogDiv.addEventListener("mouseenter",()=>{blogArticleBody.classList.add("show-shii"),articleRule.classList.add("dont-show-shii")}),blogDiv.addEventListener("mouseleave",()=>{blogArticleBody.classList.contains("show-shii")&&blogArticleBody.classList.remove("show-shii"),articleRule.classList.contains("dont-show-shii")&&articleRule.classList.remove("dont-show-shii")}));
+
+const blogArticleBody = document.getElementById("blog-article-body");
+const articleRule = document.getElementById("article-rule");
+const blogDiv = document.querySelector("#blog-main");
+const bgImg = blogDiv.getAttribute("bg-img");
+
+blogDiv.style.backgroundImage = bgImg;
+
+document.getElementById("blog-main").addEventListener("mouseenter", () => {
+  blogArticleBody.classList.add("show-shii");
+  articleRule.classList.add("dont-show-shii")
+});
+document.getElementById("blog-main").addEventListener("mouseleave", () => {
+  if (blogArticleBody.classList.contains("show-shii")) {
+    blogArticleBody.classList.remove("show-shii");
+  }
+  if(articleRule.classList.contains("dont-show-shii")){
+    articleRule.classList.remove("dont-show-shii")
+  }
+
+});
