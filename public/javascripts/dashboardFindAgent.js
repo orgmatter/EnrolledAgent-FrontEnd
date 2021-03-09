ee3b8dd11078260302a13435a6992c752e57bfd3
Chibuike Nwachukwu <chibuike@enrolledagent.org>
2021-03-09 13:23:07 +0100

    Removes hardcoded url in dashboard

--- public/javascripts/dashboardFindAgent.js
+++ public/javascripts/dashboardFindAgent.js
@@ -1,1 +1,17 @@
-const toggleBtn=document.getElementById("dropdownMenuButton"),listingForm=document.getElementById("listing-form"),select=document.getElementById("select"),zipInput=document.getElementById("zipInput"),nameInput=document.getElementById("name"),submitBtn=document.getElementById("submit-btn"),url=location.href,match=url.match("enrolledagent.org"),baseUrl=match?"https://enrolledagent.org":"http://localhost:3000";var res=!0;select.addEventListener("change",()=>{"state"===select.value?zipInput.placeholder="Enter state*":zipInput.placeholder="Enter zip code*"});const handleSubmit=e=>{e.preventDefault();const t=zipInput.value,n=nameInput.value;window.location.href=res?`${baseUrl}/search-results?q=zipcode:${t},lastName:${n}`:`${baseUrl}/search-results?q=state:${t},lastName:${n}`};listingForm.addEventListener("submit",handleSubmit);
+const toggleBtn = document.getElementById("dropdownMenuButton"),
+    listingForm = document.getElementById("listing-form"),
+    select = document.getElementById("select"),
+    zipInput = document.getElementById("zipInput"),
+    nameInput = document.getElementById("name"),
+    submitBtn = document.getElementById("submit-btn");
+var res = !0;
+select.addEventListener("change", () => {
+    "state" === select.value ? (zipInput.placeholder = "Enter state*") : (zipInput.placeholder = "Enter zip code*");
+});
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const t = zipInput.value,
+        n = nameInput.value;
+    window.location.href = res ? `/search-results?q=zipcode:${t},lastName:${n}` : `${baseUrl}/search-results?q=state:${t},lastName:${n}`;
+};
+listingForm.addEventListener("submit", handleSubmit);
